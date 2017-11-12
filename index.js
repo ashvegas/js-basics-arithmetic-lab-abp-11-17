3dfd8fd2c1908fe8f17aca7d934d5fc2b42c466d
AC <msashvegas@gmail.com>
2017-11-12 08:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ You should NOT try to redefine any of the above variables.
 const ageIsValid = Number.isInteger(currentAge)
 
 const randomNumber = Math.floor(Math.random() * 20)
+
+const randomInteger = Math.floor(randomNumber)
